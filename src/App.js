3d5b6819952e81c50f7511e629b97c5ec5c1e924
MichaelGW <taciuk73@gmail.com>
2022-02-02 13:29:10 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Clock extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return <div>{this.state.date.toLocaleTimeString()}</div>
+  }
+
+  componentDidMount() {
+    const oneSecond = 1000;
+    this.intervalID = setInterval(() => {this.setState({ date: new Date() });}, oneSecond);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+}
 
 function App() {
+
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <Clock/>
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,5 +47,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,18 @@
+import React, {Component, Fragment} from "react";
+
+class Sidebar extends Component {
+
+
+    render() {
+
+        return (
+            <div>
+                <h1>
+                    This is the Sidebar
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default Sidebar;
