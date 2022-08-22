a79eb841cb25a109b40c9a8d3d0057ab99f5c7c8
MichaelGW <taciuk73@gmail.com>
2022-08-22 12:14:32 -0400

    Sidebar sessions added

---
+++ src/components/SessionButton.js
@@ -0,0 +1,114 @@
+import React, { Component, Fragment } from "react";
+import Card from "react-bootstrap/Card";
+import "./SessionButton.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPencilAlt,
+  faCheck,
+  faTimes,
+  faTrashAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import { Input } from "@material-ui/core";
+
+class SessionButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+      editingName: "",
+      showDeleteDialog: false,
+    };
+    this.nameRef = React.createRef();
+  }
+
+  getButtonColour = () => {
+    if (this.props.selected) {
+      return "selected";
+    } else if (this.props.index % 2 == 0) {
+      return "oddCard";
+    } else {
+      return "evenCard";
+    }
+  };
+
+  handleEditNameClick = (e) => {
+    e.stopPropagation();
+    this.setState({ editing: true, editingName: this.props.name });
+    setTimeout(() => {
+      this.nameRef.current.focus();
+    }, 0);
+  };
+
+  handleNameChange = (value) => {
+    this.setState({ editingName: value });
+  };
+
+  handleSaveEdit = () => {
+    this.setState({ editing: false });
+    this.props.onPersonNameChange(this.state.editingName);
+  };
+
+  handleCancelEdit = () => {
+    this.setState({ editing: false });
+  };
+
+  handleClick = () => {
+    if (!this.state.editing) this.props.onClick();
+  };
+
+  handleDeleteClick = (e) => {
+    e.stopPropagation();
+    this.setState({ showDeleteDialog: true });
+  };
+
+  handleDelete = () => {
+    this.props.deletePerson();
+  };
+
+  handleKeydown = (e) => {
+    if (e.code === "Enter") this.handleSaveEdit();
+  };
+
+  render() {
+    const { editing } = this.state;
+    return (
+      <Fragment>
+        <Card.Header
+          className={`people-list ${this.getButtonColour()}`}
+          onClick={this.handleClick}
+        >
+          <Input
+            style={{ display: editing ? "" : "none" }}
+            value={this.state.editingName}
+            onChange={(e) => this.handleNameChange(e.target.value)}
+            inputRef={this.nameRef}
+            onKeyDown={this.handleKeydown}
+          />
+          {editing ? (
+            <Fragment>
+              <FontAwesomeIcon onClick={this.handleSaveEdit} icon={faCheck} />
+              <FontAwesomeIcon onClick={this.handleCancelEdit} icon={faTimes} />
+            </Fragment>
+          ) : (
+            <Fragment>
+              <p className="personTab">{this.props.name}</p>
+              <div className="actions-buttons">
+                <FontAwesomeIcon
+                  style={{ marginRight: "10px" }}
+                  onClick={this.handleEditNameClick}
+                  icon={faPencilAlt}
+                />
+                <FontAwesomeIcon
+                  onClick={this.handleDeleteClick}
+                  icon={faTrashAlt}
+                />
+              </div>
+            </Fragment>
+          )}
+        </Card.Header>
+      </Fragment>
+    );
+  }
+}
+
+export default SessionButton;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,44 @@
-import React, {Component, Fragment} from "react";
+import React, {Component} from "react";
 import "./Sidebar.css";
+import SessionButton from "./SessionButton.js";
 
 class Sidebar extends Component {
     
+    constructor(props) {
+        super(props);
+        this.state = {
+            sessions: [
+                {
+                    "name": "reading",
+                    "durration": 45*60
+                },
+                {
+                    "name": "cooking",
+                    "durration": 90*60
+                }
+            ]
+        }
+    }
+    
 
     render() {
-
+        
         return (
             <div className="sidebar-container">
                 <div className="header">
                     <h1 className="sidebar-text">Sessions</h1>
                 </div>
-                <div className="list-container"></div>
-                <button className="addToDo">Add Task</button>
+                <div className="list-container">
+                    {this.state.sessions.map((session, index) => {
+                        return (
+                            <SessionButton
+                                name={session.name}
+                                //index={index}
+                                //durration={user.durration}
+                            />
+                        );
+                    })}
+                </div>
             </div>
         );
     }
